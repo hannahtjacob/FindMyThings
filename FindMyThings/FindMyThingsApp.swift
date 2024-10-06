9086f2a704ee94e330875e9857b3330215bc94fb
hannahtjacob <hannahtjacob@gmail.com>
2024-10-06 18:40:09 -0400

    Initial commit

---
+++ FindMyThings/FindMyThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyThingsApp.swift
+//  FindMyThings
+//
+//  Created by Hannah Jacob on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FindMyThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
